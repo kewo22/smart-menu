35239c62823324ae75b5c3c08d64dedf541bf965
Pravin Fernando <pravinfernando3@gmail.com>
2023-12-28 00:30:31 +0530

    Initial commit

---
+++ src/app/menu/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Menu = () => {
+    return (<>
+        <div className="flex flex-row bg-black">
+            <div className="bg-slate-500 basis-1/4">1</div>
+            <div className="bg-red-900 basis-1/2">2</div>
+        </div>
+    </>);
+}
+
+export default Menu;
