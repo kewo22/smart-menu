99fcaedbb4a4dabfa8963b46bfd1069cc717b920
kewin@kazix.io <kewinf271@gmail.com>
2024-02-13 18:34:23 +0530

    styles fix

--- src/app/menu/[[...slug]]/template-1.tsx
+++ src/app/menu/[[...slug]]/template-1.tsx
@@ -5,10 +5,8 @@ type Template1Props = {
 };
 
 export default function Template1(props: Template1Props) {
-  console.log("🚀 ~ Template1 ~ props:", props.menuObj);
-  // console.log("🚀 ~ Template1 ~ props:", Object.values(props.menuObj));
   const arr: any[] = Object.values(props.menuObj);
-  console.log("🚀 ~ Template1 ~ arr:", arr);
+
   return (
     // max-w-lg
     <section className=" flex flex-col mx-auto bg-black h-svh">
@@ -16,46 +14,31 @@ export default function Template1(props: Template1Props) {
         FOOD MENU
       </h1>
 
-      {arr.map((item, i) => {
-        return (
-          <div key={i} className="text-white">
-            <div>{item.category}</div>
-            <div>
-              {item.items.map((d: any, i: number) => {
-                return (
-                  <div key={i}>
-                    {d.name} - {d.price}
-                  </div>
-                );
-              })}
+      <div className="flex flex-col gap-5">
+        {arr.map((item, i) => {
+          return (
+            <div key={i} className="text-white flex flex-col gap-1">
+              <div className="bg-orange-500 text-white w-fit px-3 py-1 rounded-md font-bold mb-2 mx-5">
+                {item.category}
+              </div>
+              <div className="mx-5">
+                <table className="text-white w-full">
+                  <tbody>
+                    {item.items.map((d: any, i: number) => {
+                      return (
+                        <tr key={i}>
+                          <td>{d.name}</td>
+                          <td className="text-right">{d.price}</td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
             </div>
-          </div>
-        );
-      })}
-
-      <div className="bg-orange-500 text-white w-fit px-3 py-1 rounded-md font-bold mb-2 mx-5">
-        MAIN COURSE
+          );
+        })}
       </div>
-      <table className="text-white mx-5">
-        <tbody>
-          <tr>
-            <td>Cheese Burger</td>
-            <td>$40</td>
-          </tr>
-          <tr>
-            <td>Cheese Sandwich</td>
-            <td>$40</td>
-          </tr>
-          <tr>
-            <td>Spicy Chicken</td>
-            <td>$40</td>
-          </tr>
-          <tr>
-            <td>Hotdog</td>
-            <td>$40</td>
-          </tr>
-        </tbody>
-      </table>
     </section>
   );
 }
